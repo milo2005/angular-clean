bc2e04be25e2810711de469d97ee3536cafa41ec
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-09-17 10:33:47 -0500

    proyect clean

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,7 @@
 import {Component, OnInit} from "@angular/core";
-import {Router,ROUTER_DIRECTIVES} from '@angular/router';
-
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector:"my-app",
-  template:"<router-outlet></router-outlet>",
-  directives:[ROUTER_DIRECTIVES]
+  template:"<h1>Titulo</h1>"
 })
 export class AppComponent {}
--- app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import {provideRouter, RouterConfig} from '@angular/router';
-
-const routes:RouterConfig = [
-    //{path:"/", component:Componente}
-];
-
-export const appRouterProviders = [provideRouter(routes)];
